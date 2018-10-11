5c0cabfd832a3e0250097ad407ef18af59fe91e1
Robin <ro7inq@gmail.com>
2018-10-11 00:29:11 -0400

    restart pj1

---
+++ pj1ex/src/pixel.java
@@ -0,0 +1,26 @@
+
+public class pixel {
+	private short red;
+	short green;
+    short blue;
+    
+    public short getred(){
+    	return red;
+    }
+    public short getgreen(){
+    	return green;
+    }
+    public short getblue(){
+    	return blue;
+    }
+    public void setred(short red){
+    	this.red=red;
+    }
+    public void setgreen(short green){
+    	this.green=green;
+    }
+    public void setblue(short blue){
+    	this.blue=blue;
+    }
+
+}
